de5f0f352b2f3116674ec1ca3a4bfec02dfb0282
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-23 16:12:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Capture
+Mindustry plugin that makes you able to capture cores by destroying them.
---
+++ src/main/java/Capture.java
@@ -0,0 +1,29 @@
+import arc.Events;
+import arc.util.Timer;
+import mindustry.content.Fx;
+import mindustry.entities.Units;
+import mindustry.game.EventType;
+import mindustry.game.Team;
+import mindustry.gen.Call;
+import mindustry.gen.Unit;
+import mindustry.mod.Plugin;
+import mindustry.world.Block;
+import mindustry.world.Tile;
+import mindustry.world.blocks.storage.*;
+
+public class Capture extends Plugin {
+    @Override
+    public void init() {
+        Events.on(EventType.BlockDestroyEvent.class, e -> {
+            if (e.tile.build instanceof CoreBlock.CoreBuild){
+                Tile tile = e.tile;
+                Team oldTeam = e.tile.build.team;
+                Block block = e.tile.build.block;
+                Unit closestEnemy = Units.closestEnemy(oldTeam, tile.worldx(), tile.worldy(), 10000000, u -> true);
+                Team newTeam = closestEnemy != null ? closestEnemy.team : Team.derelict;
+                Call.effectReliable(Fx.upgradeCore, tile.build.x, tile.build.y, block.size, newTeam.color);
+                Timer.schedule(() -> tile.setNet(block, newTeam, 0), 0.6f);
+            }
+        });
+    }
+}
