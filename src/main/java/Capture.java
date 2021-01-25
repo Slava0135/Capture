af181e262fede0abb27a8d02a95a9763787abeb7
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-25 11:14:25 +0300

    oh no

--- src/main/java/Capture.java
+++ src/main/java/Capture.java
@@ -24,10 +24,12 @@ public class Capture extends Plugin {
     @Override
     public void init() {
         Events.on(EventType.BlockDestroyEvent.class, e -> {
-            if (e.tile.build instanceof CoreBlock.CoreBuild && !Vars.state.gameOver){
+            if (e.tile.build instanceof CoreBlock.CoreBuild && !Vars.state.gameOver) {
+
                 Tile tile = e.tile;
                 Team oldTeam = e.tile.build.team;
                 Block block = e.tile.block();
+
                 Unit closestEnemy = Units.closestEnemy(oldTeam, tile.worldx(), tile.worldy(), 10000000, u -> true);
                 Team newTeam = closestEnemy != null ? closestEnemy.team : Team.derelict;
                 Call.effectReliable(Fx.upgradeCore, tile.build.x, tile.build.y, block.size, newTeam.color);
@@ -36,17 +38,19 @@ public class Capture extends Plugin {
                         + " []captured team [#" + oldTeam.color.toString() + "]"+ oldTeam.name
                         + "[] core at " + tile.x + ", " + tile.y, 5f, Align.center, 0, 0, 50 * messageQueue - 50 * messageLimit, 0);
                 messageQueue = (messageQueue + 1) % messageLimit;
+
                 Timer.schedule(() -> {
                     tile.setNet(block, newTeam, 0);
                     tile.build.health = Float.POSITIVE_INFINITY;
                     Fires.remove(tile);
-                    for (int size = 0; size <= block.size; size++) {
+                    for (int size = 1; size <= block.size; size++) {
                         for (Point2 edge : Edges.getEdges(size)) {
                             Fires.remove(Vars.world.tile(tile.x + edge.x, tile.y + edge.y));
                         }
                     }
                 }, 0.5f);
                 Timer.schedule(() -> tile.build.health = tile.block().health, 5f);
+
             }
         });
     }
